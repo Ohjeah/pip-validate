832b7662d9f057f6dff185c1b841c477ab4b65d1
Markus Quade <mquade@uni-potsdam.de>
2017-09-20 17:01:48 +0200

    use pip search to find aliases

--- test_pip_validate.py
+++ test_pip_validate.py
@@ -17,11 +17,33 @@ def test_is_std_lib():
     assert not is_std_lib("apps")
     assert not is_std_lib("")
 
+
+def is_connected():
+    import socket
+    try:
+        host = socket.gethostbyname("www.google.com")
+        socket.create_connection((host, 80), 2)
+        return True
+    except:
+        pass
+    return False
+
+
+@pytest.mark.skipif(not is_connected(), reason="Need an internet connection")
+def test_match_to_alias():
+    imports = ["dateutil"]
+    requirements = ["python-dateutil"]
+    aliases = match_to_alias(imports, requirements)
+    for i, r in zip(imports, requirements):
+        assert aliases[i] == r
+
 # self-test
+@pytest.mark.skipif(not is_connected(), reason="Need an internet connection")
 def test_collect_extern_file_imports():
     assert set(collect_extern_file_imports(NAME + ".py")) == set(["pip", "crayons"])
 
 
+@pytest.mark.skipif(not is_connected(), reason="Need an internet connection")
 def test_collect_dir_imports():
     imports = collect_dir_imports(os.path.dirname(__file__))
     for k, v in imports.items():
@@ -32,10 +54,12 @@ def test_collect_dir_imports():
         else:
             assert set(v) == set(["pip", "crayons"])
 
+
 def test_end_to_end_file():
     assert subprocess.call("pip-validate --file pip_validate.py --req requirements.txt", shell=True) == 0
 
 
+@pytest.mark.skipif(not is_connected(), reason="Need an internet connection")
 def test_end_to_end_dir():
     # dev requirements are missing and this file imports pytest
     assert subprocess.call("python pip_validate.py --dir . --req requirements.txt", shell=True) == 1
