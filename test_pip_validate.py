dc3ae4a630b9d5ada7c379861cc30cbd0fa90a32
Markus Quade <markus.quade@gmail.com>
2017-12-02 02:19:20 +0100

    fix #1

--- test_pip_validate.py
+++ test_pip_validate.py
@@ -1,4 +1,5 @@
 import os
+import ast
 import subprocess
 
 import pytest
@@ -6,40 +7,22 @@ import pytest
 from pip_validate import *
 
 is_import_test_lines = [
-    ("from .a import b", True),
+    ("from .a import b", False),
     ("from a import b", True),
     ("import a as b", True),
     ("import a.b", True),
     ("is_import(a)", False),
-    ("def is_import(line):", False),
     ('"All imports of {} are listed in {}".format(args.dir or args.file, args.req)', False)
 ]
 
 
 @pytest.mark.parametrize(["line", "result"],  is_import_test_lines)
-def test_is_import(line, result):
-    assert result == is_import(line)
+def test_ImportVisitor(line, result):
+    tree = ast.parse(line)
+    visitor = ImportVisitor()
+    visitor.visit(tree)
+    assert result == bool(len(visitor.imports))
 
-
-clean_line_test_lines = ["from .a import b", "from abc import b",
-                        "import a as b", "import a.b", "from a.b.c import d"]
-
-clean_line_test_results = None, "abc", "a", "a", "a"
-
-@pytest.mark.parametrize(["line", "result"],  zip(*[clean_line_test_lines, clean_line_test_results]))
-def test_clean_line(line, result):
-    assert result == clean_line(line)
-
-
-def test_ignore_docstrings_and_comments():
-    text = ["abc", 'e"""', "'''docstring1", "docstring2", '"""', "#comment", "def"]
-    text_wo_doc = ["abc", 'e', "def"]
-    assert text_wo_doc == list(ignore_docstrings_and_comments(text))
-
-
-def test_is_relative_import():
-    assert is_relative_import(".version")
-    assert not is_relative_import("version")
 
 def test_in_path():
     assert in_path("pip_validate", ".")
